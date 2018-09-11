5da749c77dd42132b7bf9961270ff3e39e5c9b5e
Eita Honjo <nkc.eita.honjo@gmail.com>
2018-09-11 10:52:03 +0900

    PlayerControllerを追加 横移動とジャンプは可能

---
+++ Mario/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Vector2 pos = new Vector2(0.0f, 0.0f);
+    int x = 3;//xの移動量
+    int y = 10;//yの移動量(ジャンプ)
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            pos.x -= x * Time.deltaTime; 
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            pos.x += x * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            pos.y += y * Time.deltaTime; 
+        }
+        transform.position = pos;
+	}
+}
