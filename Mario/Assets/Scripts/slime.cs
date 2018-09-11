7142820bd30b9a473c9dd9d92e3f0d493dc41ff6
Rikiya Yamamoto <nkc.yamamoto.rikiya@gmail.com>
2018-09-11 11:40:41 +0900

    基本的なスライムの移動…これどうしよう。カメラに入ってなくても動いちゃう。 後で考えます。

---
+++ Mario/Assets/Scripts/slime.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slime : MonoBehaviour {
+    public float a;
+	// Use this for initialization
+	void Start () {
+        a = 0.01f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Transform mytransform = this.transform;
+
+        Vector3 pos = mytransform.position;
+        pos.x += a;
+
+        mytransform.position = pos;
+	}
+}
