9770d36429d9f6648d12fdb2bcdbfdd68c7df914
Eita Honjo <nkc.eita.honjo@gmail.com>
2018-09-12 12:38:18 +0900

    Scriptを複数追加 全部アイテム関連です。

---
+++ Mario/Assets/Scripts/Items/FriedRice.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FriedRice : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("チャーハン");
+            Destroy(gameObject);
+        }
+    }
+    /*
+	// Use this for initialization
+	void Start () 
+    {		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {		
+	}*/
+}
---
+++ Mario/Assets/Scripts/Items/TianjinMeal.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TianjinMeal : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("天津飯");
+            Destroy(gameObject);
+        }
+    }
+    /*
+    PlayerController
+    void SetTenshinhan()
+    {
+
+    }
+	// Use this for initialization
+	void Start () 
+    {		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {		
+	}*/
+}
