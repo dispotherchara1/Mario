a5b528603a40eb418d7e5cf881236aa9fa075d14
Rikiya Yamamoto <nkc.yamamoto.rikiya@gmail.com>
2018-09-12 10:46:52 +0900

    マージで激突した分のほぞん

--- Mario/Assets/Scripts/slime.cs
+++ Mario/Assets/Scripts/slime.cs
@@ -2,35 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<< HEAD
-public class OnTriggerEnter2D : MonoBehaviour {
-=======
-public class slime : MonoBehaviour {
->>>>>>> origin/Honjo
-    public float a;
-	// Use this for initialization
-	void Start () {
-        a = 0.01f;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        Transform mytransform = this.transform;
-
-        Vector3 pos = mytransform.position;
-<<<<<<< HEAD
-        pos.x -= a;
-
-        mytransform.position = pos;
-	}
-    private OntriggerEnter2D(Collider2D col)
+    public class slime : MonoBehaviour
     {
+        public float a;
+        // Use this for initialization
+        void Start()
+        {
+            a = 0.01f;
+        }
 
+        // Update is called once per frame
+        void Update()
+        {
+            Transform mytransform = this.transform;
+
+            Vector3 pos = mytransform.position;
+            pos.x -= a;
+
+            mytransform.position = pos;
+        }
+        //   private OntriggerEnter2D(Collider2D col)
+        //   {
+
+        //   }
+        //       pos.x += a;
+
+        //       mytransform.position = pos;
+        //}
     }
-=======
-        pos.x += a;
-
-        mytransform.position = pos;
-	}
->>>>>>> origin/Honjo
-}
