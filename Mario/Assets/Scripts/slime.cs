b1abb09e23bf86c08d4afecef9a5575e27ddc275
Rikiya Yamamoto <nkc.yamamoto.rikiya@gmail.com>
2018-09-12 14:30:13 +0900

    スライムの移動(一応今のところ)おｋ

--- Mario/Assets/Scripts/slime.cs
+++ Mario/Assets/Scripts/slime.cs
@@ -2,31 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-    public class slime : MonoBehaviour
+public class slime : MonoBehaviour
+{
+    public float a;
+    //Transform mytransform;
+    int go =0;
+    // Use this for initialization
+    void Start()
     {
-        public float a;
-        // Use this for initialization
-        void Start()
-        {
-            a = 0.01f;
-        }
+        a = 0.01f;
+    }
 
-        // Update is called once per frame
-        void Update()
-        {
-            Transform mytransform = this.transform;
+    // Update is called once per frame
+    void Update()
+    {
+        if (go >= 1)
+        {        Transform mytransform = this.transform;
 
             Vector3 pos = mytransform.position;
             pos.x -= a;
 
             mytransform.position = pos;
         }
-        //   private OntriggerEnter2D(Collider2D col)
-        //   {
-
-        //   }
-        //       pos.x += a;
-
-        //       mytransform.position = pos;
-        //}
+        //Debug.Log(go);
     }
+    
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "SearchEnemy") {
+            go+=1;
+        }
+    }
+}
